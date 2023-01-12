be7c7d6a352a06f4edc4fb4c5b0ed995c98292cc
kang <kjxstar@gmail.com>
2023-01-12 17:56:46 +0800

    move entity

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(LdtkPlugin)
         .add_startup_system(setup)
-        .insert_resource(LevelSelection::Index(0))
+        .insert_resource(LevelSelection::Uid(0))
+        .register_ldtk_entity::<PlayerBundle>("Player")
+        .add_system(player_move)
         .run();
 }
 
@@ -17,4 +19,23 @@ fn setup(mut command: Commands, asset_server: Res<AssetServer>) {
         ldtk_handle: asset_server.load("samples.ldtk"),
         ..default()
     });
+}
+
+fn player_move(input: Res<Input<KeyCode>>, mut query: Query<(&mut Transform), With<Player>>) {
+    if input.pressed(KeyCode::D) {
+        for mut item in query.iter_mut() {
+            item.translation.x +=  1_f32;
+        }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Player;
+
+#[derive(Clone, Default, Bundle, LdtkEntity)]
+struct PlayerBundle {
+    #[sprite_bundle("player.png")]
+    #[bundle]
+    pub sprite_bundle: SpriteBundle,
+    pub player: Player,
 }

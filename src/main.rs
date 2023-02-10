da711583e08c55f7d80594e1f1124686b87802e5
kang <kjxstar@gmail.com>
2023-02-10 23:55:22 +0800

    wip camera

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod wasd;
 
 use animation::*;
 use collision::*;
+use components::Player;
 use player::*;
 use wasd::*;
 
@@ -41,8 +42,16 @@ fn main() {
         })
         // Ldtk 配置
         .add_plugin(LdtkPlugin)
-        .add_startup_system(setup)
         .insert_resource(LevelSelection::Uid(0))
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
+            set_clear_color: SetClearColor::FromLevelBackground,
+            ..Default::default()
+        })
+        .add_startup_system(setup)
+        .add_system(camera_fit_inside_current_level)
         .run();
 }
 
@@ -53,4 +62,69 @@ fn setup(mut command: Commands, asset_server: Res<AssetServer>) {
         ldtk_handle: asset_server.load("samples.ldtk"),
         ..default()
     });
+}
+
+const ASPECT_RATIO: f32 = 16. / 9.;
+
+pub fn camera_fit_inside_current_level(
+    mut camera_query: Query<
+        (
+            &mut bevy::render::camera::OrthographicProjection,
+            &mut Transform,
+        ),
+        Without<Player>,
+    >,
+    player_query: Query<&Transform, With<Player>>,
+    level_query: Query<
+        (&Transform, &Handle<LdtkLevel>),
+        (Without<OrthographicProjection>, Without<Player>),
+    >,
+    level_selection: Res<LevelSelection>,
+    ldtk_levels: Res<Assets<LdtkLevel>>,
+) {
+    if let Ok(Transform {
+        translation: player_translation,
+        ..
+    }) = player_query.get_single()
+    {
+        let player_translation = *player_translation;
+
+        // 只取第一个，还不知道为什么有多个
+        let (mut orthographic_projection, mut camera_transform) = camera_query.iter_mut().next().unwrap();
+
+        for (level_transform, level_handle) in &level_query {
+            if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
+                let level = &ldtk_level.level;
+                if level_selection.is_match(&0, level) {
+                    let level_ratio = level.px_wid as f32 / ldtk_level.level.px_hei as f32;
+
+                    orthographic_projection.scaling_mode = bevy::render::camera::ScalingMode::None;
+                    orthographic_projection.bottom = 0.;
+                    orthographic_projection.left = 0.;
+                    if level_ratio > ASPECT_RATIO {
+                        // level is wider than the screen
+                        orthographic_projection.top = (level.px_hei as f32 / 9.).round() * 9.;
+                        orthographic_projection.right = orthographic_projection.top * ASPECT_RATIO;
+                        camera_transform.translation.x = (player_translation.x
+                            - level_transform.translation.x
+                            - orthographic_projection.right / 2.)
+                            .clamp(0., level.px_wid as f32 - orthographic_projection.right);
+                        camera_transform.translation.y = 0.;
+                    } else {
+                        // level is taller than the screen
+                        orthographic_projection.right = (level.px_wid as f32 / 16.).round() * 16.;
+                        orthographic_projection.top = orthographic_projection.right / ASPECT_RATIO;
+                        camera_transform.translation.y = (player_translation.y
+                            - level_transform.translation.y
+                            - orthographic_projection.top / 2.)
+                            .clamp(0., level.px_hei as f32 - orthographic_projection.top);
+                        camera_transform.translation.x = 0.;
+                    }
+
+                    camera_transform.translation.x += level_transform.translation.x;
+                    camera_transform.translation.y += level_transform.translation.y;
+                }
+            }
+        }
+    }
 }

0b866bf89fa0f5b123e59f000a7ebe383f0d53c2
kang <kjxstar@gmail.com>
2023-02-11 15:46:40 +0800

    三段跳，以及地面检测

--- src/components.rs
+++ src/components.rs
@@ -2,3 +2,44 @@ use bevy::prelude::*;
 
 #[derive(Copy, Clone, Eq, PartialEq, Default, Debug, Component)]
 pub struct Player;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Component)]
+pub struct Grounded(pub bool);
+
+impl Default for Grounded {
+    fn default() -> Self {
+        Self(true)
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Component)]
+pub struct JumpFlag {
+    // 跳跃次数
+    pub jump_count: usize,
+    // 跳跃次数限制
+    jump_limit: usize,
+}
+
+impl Default for JumpFlag {
+    fn default() -> Self {
+        Self {
+            jump_count: 0,
+            // 三段跳，兄弟！
+            jump_limit: 2,
+        }
+    }
+}
+
+impl JumpFlag {
+    pub(crate) fn can_jump(&self) -> bool {
+        self.jump_count < self.jump_limit
+    }
+
+    pub(crate) fn increase_jump_count(&mut self) {
+        self.jump_count += 1;
+    }
+
+    pub(crate) fn clear(&mut self) {
+        self.jump_count = 0;
+    }
+}
--- src/player.rs
+++ src/player.rs
@@ -13,18 +13,29 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.register_ldtk_entity::<PlayerBundle>("Player")
-            .add_system(leafwing_input);
+            .add_system(leafwing_input)
+            .add_system(ground_detection);
     }
 }
 
 const MOVE_SPEED: f32 = 80.;
 
-fn leafwing_input(mut player_query: Query<(&mut Velocity, &ActionState<Action>), With<Player>>) {
+fn leafwing_input(
+    mut player_query: Query<
+        (
+            &mut Velocity,
+            &ActionState<Action>,
+            &mut JumpFlag,
+            &Grounded,
+        ),
+        With<Player>,
+    >,
+) {
     if player_query.is_empty() {
         return;
     }
 
-    let (mut velocity, action) = player_query.single_mut();
+    let (mut velocity, action, mut jump_flag, grounded) = player_query.single_mut();
 
     let speed = if action.pressed(wasd::Action::Speed) {
         MOVE_SPEED * 2.
@@ -38,15 +49,49 @@ fn leafwing_input(mut player_query: Query<(&mut Velocity, &ActionState<Action>),
     if action.pressed(wasd::Action::Right) {
         velocity.linvel.x = speed;
     }
-    if action.pressed(wasd::Action::Jump) {
+    if action.just_pressed(wasd::Action::Jump) && jump_flag.can_jump() {
         velocity.linvel.y = 160.;
+        jump_flag.increase_jump_count();
     }
+    if grounded.0 {
+        jump_flag.clear()
+    }
+}
+
+/// 太 6 了，地面检测是 y 轴几帧不动
+fn ground_detection(
+    mut player: Query<(&Transform, &mut Grounded), With<Player>>,
+    mut last: Local<(f32, isize)>,
+) {
+    if player.is_empty() {
+        return;
+    }
+
+    let (pos, mut on_ground) = player.single_mut();
+
+    if (pos.translation.y * 100.).round() == last.0 {
+        last.1 += 1;
+    } else {
+        last.1 -= 1;
+    };
+    last.1 = last.1.clamp(0, 5);
+
+    if last.1 == 5 && !on_ground.0 {
+        on_ground.0 = true;
+    } else if last.1 < 2 && on_ground.0 {
+        on_ground.0 = false;
+    }
+
+    last.0 = (pos.translation.y * 100.).round();
 }
 
 #[derive(Bundle, Default, LdtkEntity)]
 struct PlayerBundle {
     sprite: SpriteSheetBundle,
     player: Player,
+
+    jump_flag: JumpFlag,
+    grounded: Grounded,
 
     #[bundle]
     input: wasd::InputBundle,
